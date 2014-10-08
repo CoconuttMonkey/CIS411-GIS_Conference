21ffd88455b9d5fc4d7a2fb5546d25ff8ae0fb37
CoconuttMonkey <matthew.r.ondo@gmail.com>
2014-10-07 20:37:39 -0400

    - Updated header

---
+++ nav.php
@@ -0,0 +1,17 @@
+	<header id="navigation" class="row">
+		<div class="container">
+			<nav class="navbar navbar-pills navbar-left">
+				<ul>
+					<li><a href="../">9<sup>th</sup> Annual NW PA GIS Conference</a></li>
+				</ul>
+			</nav>
+			<nav class="navbar navbar-pills navbar-right">
+		        <ul>
+		            <li><a href="index.php">Home</a></li>
+		            <li><a href="schedule.php">Schedule</a></li>
+		            <li><a href="about.php">About</a></li>
+		            <li><a href="contact.php">Contact</a></li>
+		        </ul>
+		    </nav>
+		</div>
+	</header>
